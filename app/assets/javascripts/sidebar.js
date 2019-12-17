67cba4f0696671e4a4fc02d07003d20023f58588
Mko-dai <mk0829@outlook.jp>
2019-12-18 00:34:42 +0900

    js-finish

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,13 +1,34 @@
-function CloseSidebar1() {
-
+function CloseSidebar() {
+  // トップページのサイドバー非表示処理  
+  
   var Sidebar1	= document.getElementById("sidebaradd");
-  var Sidebar2	= document.getElementById("sidebarnone");
+  var Main1 = document.getElementById("mainadd");
+
   if (Sidebar1.className == "index_sidebar1"){
-    Sidebar2.className = "index_sidebar2"
-    Sidebar1.className = "index_sidebar1_display_none";
-    
+    Sidebar1.className = "display_none";
+    Main1.className = "index__main2"
+
   }else{
     Sidebar1.className = "index_sidebar1";
-    Sidebar2.className = "index_sidebar1_display_none"
+    Main1.className = "index__main1"
   }
 };
+
+function ChaneForm() {
+  // 検索フォームの切り替え
+  
+  var musicform = document.getElementById("formmusic");
+  var artistform = document.getElementById("formartist");
+  var chatgroupform = document.getElementById("formchatgroup");
+
+  if (musicform.className == "index_header__searchbar__form"){
+    musicform.className = "display_none";
+    artistform.className = "index_header__searchbar__form";
+  }else if(artistform.className == "index_header__searchbar__form"){
+    artistform.className = "display_none";
+    chatgroupform.className = "index_header__searchbar__form";
+  }else{
+    chatgroupform.className = "display_none";
+    musicform.className = "index_header__searchbar__form";
+  }
+}
