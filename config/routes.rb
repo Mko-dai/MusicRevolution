f87550a7f86594a8fd867c27f0c11b17b111c45e
Mko-dai <mk0829@outlook.jp>
2019-12-23 19:02:21 +0900

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:"audios#index"
-  resources :audios,only: [:index,:show]
-
+  
+  resources :audios
+  resources :groups do
+    resources :chats,only: [:index,:create]
+  end
 
 end
