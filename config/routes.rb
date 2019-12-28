88fdf8be683416072191f081dfb363e1cb8a6b9e
Mko-dai <mk0829@outlook.jp>
2019-12-29 00:22:48 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to:"audios#index"
-  
+  resources :users,only: [:show]
   resources :audios
   resources :groups do
     resources :chats,only: [:index,:create]
   end
-
+  
 end
