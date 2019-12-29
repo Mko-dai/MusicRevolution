a2b14f33ffb4ac04f223f5284bcf97652b95314d
Mko-dai <mk0829@outlook.jp>
2019-12-29 15:50:50 +0900

    矢印変化

---
+++ app/assets/javascripts/usershow.js
@@ -0,0 +1,26 @@
+function Usershow2Bar1() {
+  var yajirushi = document.getElementById("toukouyajirushi2");
+  if ( yajirushi.className == "fas fa-chevron-down" ) {
+    yajirushi.className = "fas fa-chevron-up";
+  }else{
+    yajirushi.className = "fas fa-chevron-down";
+  }
+};
+
+function Usershow2Bar2() {
+  var yajirushi = document.getElementById("toukouyajirushi3");
+  if ( yajirushi.className == "fas fa-chevron-down" ) {
+    yajirushi.className = "fas fa-chevron-up";
+  }else{
+    yajirushi.className = "fas fa-chevron-down";
+  }
+};
+
+function Usershow2Bar3() {
+  var yajirushi = document.getElementById("toukouyajirushi4");
+  if ( yajirushi.className == "fas fa-chevron-down" ) {
+    yajirushi.className = "fas fa-chevron-up";
+  }else{
+    yajirushi.className = "fas fa-chevron-down";
+  }
+};
