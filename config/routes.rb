8f85a79dcfe07eb0623d8315468638e6c6369fc5
Mko-dai <mk0829@outlook.jp>
2019-12-29 18:17:05 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to:"audios#index"
   resources :users,only: [:show]
   resources :audios
+  resources :artists,only: [:show]
   resources :groups do
     resources :chats,only: [:index,:create]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_28_081702) do
+ActiveRecord::Schema.define(version: 2019_12_29_085524) do
+
+  create_table "artists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "audios", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
