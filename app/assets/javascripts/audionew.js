25791f7817150039e0f934b3661a4c9db0fa45fd
Mko-dai <mk0829@outlook.jp>
2020-01-11 14:22:01 +0900

    finish

---
+++ app/assets/javascripts/audionew.js
@@ -0,0 +1,13 @@
+// 画像のプレビュー表示
+
+// document.getElementById("imageupload").addEventListener('change', function (e) {
+//   // 1枚だけ表示する
+//   var file = e.target.files[0];
+//   console.log(file);
+//   // ファイルのブラウザ上でのURLを取得する
+//   var blobUrl = window.URL.createObjectURL(file);
+
+//   // img要素に表示
+//   var img = document.getElementById("imagepreview");
+//   img.src = blobUrl;
+// });
